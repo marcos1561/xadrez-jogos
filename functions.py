7aef5a4b7a6e7fe87bee639f9e68a3a9265155cc
Marco Pasa <marcospasa130@gmail.com>
2020-12-04 19:20:36 -0300

    Algoritmo finalizado, mas ainda falta toda a documentação

--- functions.py
+++ functions.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def exibir_rodadas(rodadas, max_duplas, long_el):
     """
     Imprime no console o conjunto que possui as rodadas.
@@ -34,15 +36,33 @@ def exibir_rodadas(rodadas, max_duplas, long_el):
 
 
 def get_list_item(arr, arr_id):
+    """
+    Retorna o elemento da lista "arr" cujo índice é arr_id[0], arr_id[1], arr_id[2], ... arr_id[n], onde n = len(arr_id)-1
+
+    Parâmetros
+    ----------
+    arr: list 
+        lista de qualquer tamanho
+    
+    arr_id: list
+        Lista contente os índices do elemento para retorna. Se tamanho deve ser menor ou igual a dimensão de "arr".
+
+    Retorna
+    -------
+    element_return: list ou o tipo de elemento que estiver em "arr"
+        Elemento da lista "arr" cujo índice é arr_id[0], arr_id[1], arr_id[2], ... arr_id[n], onde n = len(arr_id)-1
+    """
+
     for index in arr_id:
         next_arr = arr[index]
         arr = next_arr
     
-    return arr
+    element_return = arr
+    return element_return
 
 
 def duplas_to_ignore(duplas_ignore, jogo_num):
-    arr_id = []
+    arr_id = [-1]
 
     for i in range(jogo_num):
         arr_id[0] = i
@@ -50,25 +70,43 @@ def duplas_to_ignore(duplas_ignore, jogo_num):
         arr_id.append(next_id)
 
     arr_id[0] += 1
-    return get_list_item(duplas_ignore, arr_id)
+    return (arr_id)
+
+
+def duplas_update_shape(duplas_ignore, arr_id): 
+    jogo_num = arr_id[0]
+    max_jogos = len(duplas_ignore)
+
+    dim = 0
+    for i in range(jogo_num+1, max_jogos):
+        shape = [1 for _ in range(dim)] + [0]
+        dim += 1 
+        
+        arr_id[0] = i
+        duplas_now = get_list_item(duplas_ignore, arr_id)
+        duplas_now.append(np.empty(shape=shape).tolist())
 
 
 def verificar(rodadas, n):
+    error = False
     duplas = []
 
     for rodada in rodadas:
         if len(rodada) != int(n/2):
+            error = True
             print(f"Erro! Número de jogos na rodada.\n"
                   f"Esperado {int(n/2)} jogos ao invés de {len(rodada)}.\n"
                   f"Rodada: {rodada}\n")    
 
         elementos = []
         for dupla in rodada:
-            el_i = [dupla[:1], dupla[-1:]]
+            x_id = dupla.find("X")
+            el_i = [dupla[:x_id-1], dupla[x_id+2:]]
             for el in el_i:
                 if el not in elementos:
                     elementos.append(el)
                 else:
+                    error = True
                     print(f"Erro! Elemento repetido\n"
                           f"Elemento: {el}\n"
                           f"Rodada: {rodada}\n")
@@ -76,6 +114,9 @@ def verificar(rodadas, n):
             if dupla not in duplas:
                 duplas.append(dupla)
             else:
+                error = True
                 print(f"Erro! Dupla repetida\n"
                       f"Dupla: {dupla}\n")
 
+    if not error:
+        print("Todas as rodadas estão corretas! :)")
--- xadrez.py
+++ xadrez.py
@@ -1,8 +1,11 @@
 import numpy as np
+import time
 from functions import *
 
+start_time = time.time()
+
 # Número de elementos
-num_el = 6
+num_el = 22
 elementos = np.arange(num_el)
 
 
@@ -16,6 +19,8 @@ for i in range(elementos.size):
     for j in range(i+1, elementos.size):
         duplas = np.append(duplas, f"{i} X {j}")
 
+print(len(duplas))
+
 
 ''' Separa o conjunto das duplas em rodadas. 
     
@@ -28,56 +33,91 @@ rodadas = []
 for i in range(num_el-1):
     rodadas.append([])
 
-np.random.shuffle(duplas)
+print(len(rodadas))
+
+# np.random.shuffle(duplas)
 duplas_copia = list(duplas)
+
+# print(duplas)
+# duplas_copia = ['0 X 1', '1 X 5', '0 X 4','3 X 4', '4 X 5', '0 X 2', '0 X 5', '2 X 4', '1 X 3','1 X 4', '0 X 3', '2 X 3', '2 X 5', '1 X 2', '3 X 5']
+
 for rodada in rodadas:
     duplas_selec = []
     el_selec = []
 
+    # duplas_ignore = []
+    # for i in range(int(num_el/2)):
+    #     jogo_i_ignore = []
+    #     for j in range(i):
+    #         jogo_i_ignore.append([])
+        
+    #     duplas_ignore.append(jogo_i_ignore) 
+
+    contador = 0
+
     duplas_ignore = []
     for i in range(int(num_el/2)):
-        jogo_i_ignore = []
-        for j in range(i):
-            jogo_i_ignore.append([])
-        
-        duplas_ignore.append(jogo_i_ignore) 
+        shape = [1 for _ in range(i)] + [0]
+        new_dlp_ig = np.empty(shape=shape).tolist()
+        duplas_ignore.append(new_dlp_ig)
+    
 
     i = 0 
     while i < len(duplas_copia):
+        contador += 1
+
         dupla = duplas_copia[i]
         
-        current_dupla_ignore = duplas_to_ignore(duplas_ignore, len(duplas_selec))
+        dupla_ignore_arr_id = duplas_to_ignore(duplas_ignore, len(duplas_selec))
+        current_dupla_ignore = get_list_item(duplas_ignore, dupla_ignore_arr_id)
+        
         if dupla not in current_dupla_ignore:
-            el_1 = dupla[:1]
-            el_2 = dupla[-1:]
+            x_id = dupla.find("X")
+            el_1 = dupla[:x_id-1]
+            el_2 = dupla[x_id+2:]
             
             el_selec = []
             for item in duplas_selec:
-                [el_selec.append(el) for el in [item[:1], item[-1:]]]
+                x_id = item.find("X")
+                [el_selec.append(el) for el in [item[:x_id-1], item[x_id+2:]]]
 
             if el_1 not in el_selec and el_2 not in el_selec:
                 duplas_selec.append(dupla)
                 
                 rodada.append(dupla)
                 
-                exibir_rodadas(rodadas, int(num_el/2), num_el)
-                print(f"{dupla}\n", "=-="*30)
+            # if contador > 5000:
+            #     # if len(rodadas[13]) == 6:
+            #     #     noice = 2
+
+            #     contador = 0
+            #     exibir_rodadas(rodadas, int(num_el/2), num_el)
+            #     print(f"{dupla}\n", "=-="*30)
+            #     # print(duplas_copia)
             
             if len(rodada) == int(num_el/2):
                 break
 
         i += 1
         if i == len(duplas_copia):
-            
             i = 0
-            
-            
+
+            dpl_up_id = duplas_to_ignore(duplas_ignore, len(duplas_selec)-1)
+            dpl_up = get_list_item(duplas_ignore, dpl_up_id)
+            dpl_up.append(duplas_selec[-1])
+
+            duplas_update_shape(duplas_ignore, dpl_up_id)
 
             duplas_selec.pop(-1)
             rodada.pop(-1)
-            
 
     [duplas_copia.remove(dupla) for dupla in duplas_selec]
 
+
+end_time = time.time()
+exec_time = end_time - start_time
+
+print(duplas_copia)     
+exibir_rodadas(rodadas, int(num_el/2),  num_el)
 verificar(rodadas, num_el)
-print(rodadas)     
+print(f"{round(exec_time, 2)} segundos")
