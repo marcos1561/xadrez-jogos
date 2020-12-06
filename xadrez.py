34ef5843d545adcd3221b937c29951ab0f9107f7
marcos1561 <65289708+marcos1561@users.noreply.github.com>
2020-12-06 17:14:22 -0300

    Descartado o uso da variável duplas_ignore Apenas uma pequena modicação no momento de rertirar uma dupla na rodada, quando a mesma é última da lista, foi necessário para essa mudança. Se deve excluir as duas últimas duplas da rodada e continuar o mesmo processo.

--- xadrez.py
+++ xadrez.py
@@ -5,11 +5,11 @@ from functions import *
 start_time = time.time()
 
 # Número de elementos. OBS: Essa variável deve ser um número par.
-num_el = 26
+num_el = 20
 elementos = np.arange(num_el)
 
 #Exibir rodada sendo preenchida no console?
-exibir_rod_console = True
+exibir_rod_console = False
 # Controlam a frequência da exibição da rodada no console
 count_max = 50000
 
@@ -48,24 +48,6 @@ duplas_copia = list(duplas)
 for rodada in rodadas:
     # Contém os elementos selecionados da rodada em questão
     el_selec = []
-    # Sempre que é adicionado ou removido algum jogo da rodada, essa variável fica = True
-    len_rodada_change = True
-
-    """
-        Essa variável contém todas as listas com duplas para serem ignoradas (Uma dupla deve ser ignorada caso 
-        seja impossível completar a rodada logo após ela ter sido adicionada na rodada).
-
-        OBS: ver documentação da função "duplas_to_ignore_id" para mais informações
-    """
-    duplas_ignore = []
-    # Gera todos os elementos de "duplas_ignore" com saus respectivas dimensões.
-    # Conforme o algoritmo for executado, duplas serão adicionadas nessa variável.
-    # Quando a rodada for completa, essa variável será limpada.
-    # num_el/2 é o número de jogos que cada rodada vai possuir.
-    for i in range(int(num_el/2)):
-        shape = [1 for _ in range(i)] + [0]
-        new_dlp_ig = np.empty(shape=shape).tolist()
-        duplas_ignore.append(new_dlp_ig)
 
     # Loop que preenche a rodada
     # Pega dupla por dupla e tenta colocar na rodada em questão
@@ -75,36 +57,27 @@ for rodada in rodadas:
         contador += 1
         dupla = duplas_copia[i]
         
-        # Caso o compriemnto da rodada mude, é necessário recalcular qual deve ser a lista com duplas para ignorar
-        if len_rodada_change:
-            # Baseada na posição em que estamos tentanto colocar a dupla na rodada (len(rodada)), pega a lista de duplas para ignorar,
-            # ou seja, as duplas que tentaram ser colocada nessa rodada, mas não deram certo
-            dupla_ignore_arr_id = duplas_to_ignore_id(duplas_ignore, len(rodada))
-            current_dupla_ignore = get_list_item(duplas_ignore, dupla_ignore_arr_id)
-            len_rodada_change = False
+        # Pega os elementos da dupla selecionada
+        x_id = dupla.find("X")
+        el_1 = dupla[:x_id-1]
+        el_2 = dupla[x_id+2:]
         
-        if dupla not in current_dupla_ignore:
-            # Pega os elementos da dupla selecionada
-            x_id = dupla.find("X")
-            el_1 = dupla[:x_id-1]
-            el_2 = dupla[x_id+2:]
-            
-            # Se ambos elementos da dupla seleciona não estiverem nos elementos da rodada, adiciona a dupla na rodada
-            if el_1 not in el_selec and el_2 not in el_selec:
-                # Coloca os elementos dessa dupla em "el_selec"
-                [el_selec.append(el) for el in [el_1, el_2]]
-                rodada.append(dupla)
-                len_rodada_change = True
+        # Se ambos elementos da dupla seleciona não estiverem nos elementos da rodada, adiciona a dupla na rodada
+        if el_1 not in el_selec and el_2 not in el_selec:
+            # Coloca os elementos dessa dupla em "el_selec"
+            [el_selec.append(el) for el in [el_1, el_2]]
+            rodada.append(dupla)
+            len_rodada_change = True
 
-                # Se a rodada estiver completa, quebra o Loop e vai para a próxima rodada
-                if len(rodada) == int(num_el/2):
-                    break
+            # Se a rodada estiver completa, quebra o Loop e vai para a próxima rodada
+            if len(rodada) == int(num_el/2):
+                break
 
-            # Exibe as rodadas como estão no momento, no console
-            if contador > count_max and exibir_rod_console:
-                contador = 0
-                exibir_rodadas(rodadas, int(num_el/2), num_el)
-                print(f"Dupla atual: {dupla}\n", "=-="*30)
+        # Exibe as rodadas como estão no momento, no console
+        if contador > count_max and exibir_rod_console:
+            contador = 0
+            exibir_rodadas(rodadas, int(num_el/2), num_el)
+            print(f"Dupla atual: {dupla}\n", "=-="*30)
             
         i += 1
 
@@ -114,28 +87,20 @@ for rodada in rodadas:
             # Calcula em qual ponto do loop se deve voltar para continuar preenchendo a rodada.
             # Esse ponto deve ser o índice da dupla que está sendo removida mais 1.
             # Como esse loop nunca deve acabar sem preencher a rodada, caso a dupla a ser removida seja a última na lista,
-            # o ponto no loop continua sendo o último ponto.
+            # o ponto para retornar o loop é o índice da penúltima duplas na rodada mais 1 (OBS: Nessa caso a última e a
+            # penúltima duplas na rodada devem ser removidas. Uma é removida nesse if e a outra logo após)
             next_i = duplas_copia.index(rodada[-1]) + 1
             if next_i == len(duplas_copia):
-                next_i = len(duplas_copia)-1
+                del(el_selec[-2:])
+                rodada.pop(-1)
+                next_i = duplas_copia.index(rodada[-1]) + 1
 
             i = next_i
-
-            # índice da lista que contém as duplas para ignorar em que será adiciona a última dupla da rodada em questão
-            dpl_up_id = duplas_to_ignore_id(duplas_ignore, len(rodada)-1)
-            # Lista que contém as duplas para ignorar em que será adiciona a última dupla da rodada em questão
-            dpl_up = get_list_item(duplas_ignore, dpl_up_id)
-            # Adicionado a última dupla na rodada na lista de duplas para ignorar
-            dpl_up.append(rodada[-1])
-
-            # Atualiza os shapes da listas que estão em "duplas_ignore" pois um item for adiciona em alguma de suas listas
-            duplas_update_shape(duplas_ignore, dpl_up_id)
 
             # Retira a última dupla da rodada e os dois últimos elementos de "el_selec"
             del(el_selec[-2:]) 
             rodada.pop(-1)
-            len_rodada_change = True
-
+        
     # Remove as duplas que foram colocadas na rodada, na lista que contém todos os jogos possíveis.
     [duplas_copia.remove(dupla) for dupla in rodada]
 
